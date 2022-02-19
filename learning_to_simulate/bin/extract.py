cacacbf45e9c5411b38680794cd2f85c54276fc4
kawa-work <k.kawakami.work@gmail.com>
2022-02-19 10:29:18 +0900

    chore: change data file path

--- learning_to_simulate/bin/extract.py
+++ learning_to_simulate/bin/extract.py
@@ -1,7 +1,7 @@
 import re
 import matplotlib.pyplot as plt
 
-with open("/tmp/loss/sample.txt", mode="r") as f:
+with open("learning_to_simulate/loss.log", mode="r") as f:
     s = f.read()
 
 ext = re.findall('INFO:tensorflow:loss = ([0-9.]*), step = ([0-9]*)', s)
@@ -10,5 +10,5 @@ y, x = zip(*ext)
 x = [float(s) for s in x]
 y = [float(s) for s in y]
 plt.plot(x, y)
-plt.savefig('loss.png')
+plt.savefig('learning_to_simulate/loss.png')
 
